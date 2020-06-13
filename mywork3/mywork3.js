134abdedac908c7ef643a64a46438b5a79bc951a
vladislav <vipvladislav@gmail.com>
2020-06-13 21:11:57 +0300

    init

---
+++ mywork3/mywork3.js
@@ -0,0 +1,16 @@
+'use strict'
+// let confRes = confirm('Оно Тебе надо?');
+// console.log(confRes);
+// let promptRes = prompt('Тебе есть?','18');
+// console.log(promptfRes);
+
+
+// let promptRes = prompt('сколько Вам лет', 10);
+// if(promptRes<=10){
+//     alert('еще малыш')
+// }
+// else if(promptRes <=18){
+//     alert('поздравляю, Вы вподросток')}
+// else {
+//     alert('поздравляю, Вы большой')
+// };
